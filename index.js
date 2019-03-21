b9cef400f3471d85282a764f62eca258f9884d01
islam-nasr <islam-mohamed@windowslive.com>
2019-03-21 22:05:12 +0200

    Admin only ya gma3a

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
 const express = require("express");
+const mongoose = require("mongoose");
+// Require Router Handlers
+const admins = require("./routes/api/admins");
+const candidates = require("./routes/api/candidates");
+const partners = require("./routes/api/partners");
+const certificates = require("./routes/api/certificates");
+const consultancies = require("./routes/api/consultancies");
+// express
 const app = express();
-const conversations = require("./routes/api/conversations");
-app.use("/conversations", conversations);
-app.set("port", process.env.PORT || 5000);
-app.get("/", (req, res) => {
-  res.send('<a href="/conversations">conversations</a>');
-});
-app.listen(app.get("port"), function() {
-  console.log("App is running, server is listening on port ", app.get("port"));
-});
+// DB Config
+const db = require("./config/keys").mongoURI;
+// Connect to mongo
+mongoose
+  .connect(db)
+  .then(() => console.log("Connected to MongoDB"))
+  .catch(err => console.log(err));
+// Init middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+// Entry point
+app.get("/", (req, res) => res.send(`<h1>Welcome to LirtenHub</h1>`));
+app.get("/test", (req, res) => res.send(`<h1>Deployed on Heroku</h1>`));
+// Direct to Route Handlers
+app.use("/api/partners", partners);
+app.use("/api/candidates", candidates);
+app.use("/api/admins", admins);
+app.use("/api/consultancies", consultancies);
+app.use("/api/certificates", certificates);
+// wrong path
+app.use((req, res) =>
+  res.status(404).send(`<h1>Can not find what you're looking for</h1>`)
+);
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`Server on ${port}`));
