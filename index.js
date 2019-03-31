5f4e0022d31b72fdbac19ddd6d49a3753cb6be27
PeterAbdalla <peter-adel-2007@hotmail.com>
2019-03-31 02:16:50 +0200

    sprint 2 heroku

--- index.js
+++ index.js
@@ -12,11 +12,9 @@ const partnersConversations = require("./workflow/partner/conversations");
 const consultanciesConversations = require("./workflow/consultancy/conversations");
 // express1
 const app = express();
-// DB Config
-const db = require("./config/keys").mongoURI;
 // Connect to mongo
 mongoose
-  .connect(db, { useNewUrlParser: true })
+  .connect("mongodb://localhost:27017/SE", { useNewUrlParser: true })
   .then(() => console.log("Connected to MongoDB"))
   .catch(err => console.log(err));
 // Init middleware
@@ -43,4 +41,3 @@ app.use((req, res) =>
 );
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server on ${port}`));
-//
