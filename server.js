4e55e1fa5da9d0a1fda45844e2eb36f014faae30
karimsalahh <kareem-hafez@hotmail.com>
2019-04-14 18:02:35 +0200

    make it better

--- server.js
+++ server.js
@@ -49,6 +49,6 @@ if (process.env.NODE_ENV === "production") {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 }
-// Port
+// Port number
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server started on port ${port}`));
