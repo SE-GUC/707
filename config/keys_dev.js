7d6e29d16962ee15e0e3200d3277ca6dee9092b7
ahmedshaker07 <ahmed_hossam_012@yahoo.com>
2019-04-15 00:35:23 +0200

    MONGODB ATLAS LINK

--- config/keys_dev.js
+++ config/keys_dev.js
@@ -1,5 +1,5 @@
 module.exports = {
   mongoURI:
-    "mongodb://localhost/SE",
+    "mongodb+srv://PeterAbdalla:LirtenHub707@lirtenhub-707-cluster-dmyn2.mongodb.net/test?retryWrites=true",
   secretOrKey: "verysecretkey"
 };
