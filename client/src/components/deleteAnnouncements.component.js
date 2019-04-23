5405067d82fb4708ed55f1e588913dcc3255512b
farahyasserr <farah.dawoudd@gmail.com>
2019-04-23 03:27:36 +0200

    User stories done- Islam Update Awaiting Tasks Done

--- client/src/components/deleteAnnouncements.component.js
+++ client/src/components/deleteAnnouncements.component.js
@@ -92,6 +92,49 @@ export default class deleteAnnouncements extends Component {
           })
 
       }
+      
+      onSubmit2(id) {
+
+        const cookies = new Cookies();
+
+        const token= cookies.get('token');
+
+        console.log(token)
+
+        axios.get('http://localhost:5000/api/admins/announcement/'+ id, { headers: {
+
+            Authorization: token}
+
+          })
+
+          .then(res => {
+
+            const announcements = res.data.data;
+            this.setState({announcements:[announcements]})
+            console.log(announcements);
+            this.rerender2(token,id);
+
+          })
+
+    }
+    rerender2(token,id) {
+        axios.get('http://localhost:5000/api/admins/announcement/'+id, { headers: {
+
+            Authorization: token}
+
+          })
+
+
+          .then(res => {
+            const certificates = [res.data.data];
+
+            this.setState({ certificates });
+
+            console.log(certificates);
+
+          })
+
+      }
 
    
 
@@ -111,6 +154,7 @@ export default class deleteAnnouncements extends Component {
                <br></br>
 
                 <button type="button" className="btn btn-danger" onClick={this.onSubmit.bind(this, announcement._id)}>Delete</button>
+               <sp> </sp> <button type="button" className="btn btn-success" onClick={this.onSubmit2.bind(this, announcement._id)}>Get announcement</button><br></br>
 
                 </p>
 
--- client/src/components/partner-awaiting-approval.component.js
+++ client/src/components/partner-awaiting-approval.component.js
@@ -3,7 +3,9 @@ import React, { Component } from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router, NavLink } from "react-router-dom";
 import Cookies from "universal-cookie";
+import PartnerTasks from "./admin-tasks.component.js";
 import AddTask from "./add-task.component.js";
+
 export default class projects extends Component {
   constructor(props) {
     super(props);
@@ -33,6 +35,96 @@ export default class projects extends Component {
       document.getElementById("root")
     );
   }
+  delete(id) {
+    const cookies = new Cookies();
+    const token = cookies.get("token");
+    axios
+      .delete("http://localhost:5000/api/partners/project/" + id, {
+        headers: {
+          Authorization: token
+        }
+      })
+      .then(res => {
+        if(res.data.msg==='This project has been deleted successfully'){      
+          alert('Deleted Successfully!');
+        }
+
+      }).then(res => {
+        axios.put('http://localhost:5000/api/partners/update/projects',{},{
+            headers: {
+                Authorization: token
+            }
+        }).catch(error=>{ alert('Project cannot be deleted!')});
+    window.location.reload();
+    })}
+      getmethod(Projectid) {
+
+        const cookies = new Cookies();
+    
+        const token= cookies.get('token');
+    
+        console.log(token)
+    
+        axios.get('http://localhost:5000/api/partners/project/'+ Projectid, { headers: {
+    
+            Authorization: token}
+    
+          })
+    
+          .then(res => {
+            const projects = res.data.data;
+            this.setState({pendingProjects:[projects]})
+            console.log(projects);
+            this.rerender2(token,Projectid);
+    
+          })
+    
+    }
+    
+rerender2(token,Projectid) {
+  axios.get('http://localhost:5000/api/partners/project/'+Projectid, { headers: {
+
+      Authorization: token}
+
+    })
+
+
+    .then(res => {
+      const projects = [res.data.data];
+      this.setState({ projects });
+
+    })
+
+}
+    rerender(token) {
+      axios
+        .get("http://localhost:5000/api/partners/pendingProjects", {
+          headers: {
+            Authorization: token
+          }
+        })
+        .then(res => {
+          const pendingProjects = res.data.data;
+          this.setState({ pendingProjects });
+        });
+    }
+    showtasks = id => {
+      window.location.replace("/partnershowAwaitingtasks/"+id)
+    };
+     
+     update(token){
+       axios
+         .put("http://localhost:5000/api/partners/update/projects",{
+         headers:{
+           Authorization: token
+         }
+       })
+       .then(res => {
+         if(res.data.msg==='all projects are updated'){
+              alert('updated successfully!')
+         }
+       });
+     }
   
   render() {
     return (
@@ -84,6 +176,17 @@ export default class projects extends Component {
                 <div className="form-group">
                         <input type="submit" value="Add Task" className="btn btn-primary" onClick={this.addTask.bind(this,project._id)}/>
                     </div>
+                    <div className="form-group">
+                        <input type="submit" value="Get Project" className="btn btn-primary" onClick={this.getmethod.bind(this,project._id)}/>
+                    </div>
+                    <div className="form-group">
+                        <input type="submit" value="Delete" className="btn btn-primary" onClick={this.delete.bind(this,project._id)}/>
+                    </div>
+                    <div className="form-group">
+                        <input type="submit" value="Show Tasks" className="btn btn-primary" onClick={this.showtasks.bind(this,project._id)}/>
+                    </div>
+
+                    
                 <br />
                 
               </p>
